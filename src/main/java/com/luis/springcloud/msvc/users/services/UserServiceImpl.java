694f11c3a23ca29579d1ef5ea627f669cd48e02d
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-03-23 00:12:43 -0600

    Create RoleRepository and modify save method to find role on UserServiceImpl service.

--- src/main/java/com/luis/springcloud/msvc/users/services/UserServiceImpl.java
+++ src/main/java/com/luis/springcloud/msvc/users/services/UserServiceImpl.java
@@ -1,50 +1,61 @@
 package com.luis.springcloud.msvc.users.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.luis.springcloud.msvc.users.entities.Role;
 import com.luis.springcloud.msvc.users.entities.User;
+import com.luis.springcloud.msvc.users.repositories.RoleRepository;
 import com.luis.springcloud.msvc.users.repositories.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService{
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository repository) {
-        this.repository = repository;
+    private final RoleRepository roleRepository;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
     @Transactional(readOnly = true)
     public Optional<User> findById(Long id) {
-        return this.repository.findById(id);
+        return this.userRepository.findById(id);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Optional<User> findByUsername(String username) {
-        return this.repository.findByUsername(username);
+        return this.userRepository.findByUsername(username);
     }
 
     @Override
     @Transactional
     public User save(User user) {
-        return this.repository.save(user);
+        List<Role> roles = new ArrayList<>();
+        Optional<Role> roleOptional = this.roleRepository.findByName("ROLE_USER");
+        roleOptional.ifPresent(role -> roles.add(role));
+        user.setRoles(roles);
+        return this.userRepository.save(user);
     }
 
     @Override
     @Transactional
     public void delete(Long id) {
-        this.repository.deleteById(id);
+        this.userRepository.deleteById(id);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Iterable<User> findAll() {
-        return this.repository.findAll();
+        return this.userRepository.findAll();
     }
 
 }
