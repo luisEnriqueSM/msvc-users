121fc6008952c11e37712ac9af1014dc921317e3
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-04-11 12:40:54 -0600

    Add logs on UserController for Micrometer

--- src/main/java/com/luis/springcloud/msvc/users/controllers/UserController.java
+++ src/main/java/com/luis/springcloud/msvc/users/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.luis.springcloud.msvc.users.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +20,8 @@ import com.luis.springcloud.msvc.users.services.UserService;
 @RestController
 public class UserController {
 
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -26,29 +30,34 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> create(@RequestBody User user){
+        logger.info("UserController::create: creando {}", user);
         return ResponseEntity.status(HttpStatus.CREATED).body(this.userService.save(user));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<User> update(@RequestBody User user, @PathVariable Long id){
+        logger.info("UserController::update: actualizando {}", user);
         return this.userService.update(user, id).map(userUpdated -> ResponseEntity.ok().body(userUpdated))
                     .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id){
+        logger.info("UserController::getUserById: obteniendo con Id {}", id);
         return this.userService.findById(id).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/username/{username}")
     public ResponseEntity<User> getUserByUserName(@PathVariable String username){
+        logger.info("UserController::getUserByUserName: login con {}", username);
         return this.userService.findByUsername(username).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping
     public ResponseEntity<Iterable<User>> list(){
+        logger.info("UserController::list: listando usuarios");
         return ResponseEntity.ok().body(this.userService.findAll());
     }
 
@@ -57,6 +66,7 @@ public class UserController {
         Optional<User> user = this.userService.findById(id);
         if(user.isPresent()){
             this.userService.delete(id);
+            logger.info("UserController::delete: eliminando user con id {}", id);
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
